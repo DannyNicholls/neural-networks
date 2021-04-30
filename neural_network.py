b32caf14fa12dd70ff32e1bacf766f3b5f4e68d7
Danny Nicholls <danny.nicholls@gmail.com>
2021-04-30 09:46:09 +0100

    Added a multi-class-classification test that trains a network to predict the output of OR, AND, and XOR gates.

--- neural_network.py
+++ neural_network.py
@@ -1,4 +1,4 @@
-'''
+"""
 I have used the following abbreviations which I hope makes the code easier
 to read:
 
@@ -15,8 +15,12 @@ to read:
     dAdZ   Differential of the non-linear activations w.r.t the linear
            activations
 
+The matrix dimensions are aligned such that rows represent nodes and columns
+represent examples.  One exception being the weight matrices, for which the
+rows represent nodes in layer l - 1 and the columns represent nodes in layer l.
+
 Have a great day!
-'''
+"""
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -24,16 +28,23 @@ import pandas as pd
 
 
 def initialise_model_parameters(layer_dimensions, seed=42):
+    """Return a dictionary of random weight values.  For each layer, l,
+    greater than 1, the returned dictionary includes a key, Wl, to a numpy
+    array storing weights for that layer, and a key, bl, to a numpy array
+    storing a bias vector for that layer."""
     np.random.seed(seed)
     number_of_layers = len(layer_dimensions)
     model_parameters = {}
     for layer in range(1, number_of_layers):
         number_of_inputs = layer_dimensions[layer - 1]
         number_of_nodes = layer_dimensions[layer]
-        W = np.random.randn(number_of_inputs, number_of_nodes) * 0.01
-        b = np.zeros((number_of_nodes, 1))
+
+        W = np.random.randn(number_of_inputs, number_of_nodes)
+        b = np.random.randn(number_of_nodes, 1)
+
         model_parameters["W" + str(layer)] = W
         model_parameters["b" + str(layer)] = b
+
     return model_parameters
 
 
@@ -45,12 +56,17 @@ def linear_prediction(W, input_activation, b):
 def compute_activation(Z, activation_function):
     if activation_function == 'relu':
         A = np.fmax(0, Z)
+
     elif activation_function == 'sigmoid':
         A = 1 / (1 + np.exp(-Z))
+
     return A
 
 
 def forward(X, model_parameters, activation_functions):
+    """Return a dictionary of activation values.  For each layer, l, greater
+    than 1, the dictionary inlcudes a key, Al, to numpy array storing the
+    activations for that layer."""
     number_of_layers = (len(model_parameters) + 2) // 2
     activations = {'A0': X}
 
@@ -73,16 +89,17 @@ def forward(X, model_parameters, activation_functions):
 
 def compute_cost(Yhat, Y):
     m = Y.shape[1]
-    zero_offset = 1e-3
-    cost = -1/m * np.sum(Y * np.log(Yhat + zero_offset)
-                         + (1 - Y) * np.log(1 - Yhat + zero_offset))
 
-    # The following assumes that the output layer is sigmoid.  Need to
-    # parameterize this.
-    dYhat = - (np.divide(Y, Yhat + zero_offset)
-               - np.divide(1 - Y, 1 - Yhat + zero_offset))
+    # Add an offset to ensure that we do not calculate the log of zero.
+    offset = 1e-6
+    Yhat += offset
 
-    return cost, dYhat
+    # Cross-entropy cost function.
+    cost = (-1/m
+            * np.sum(np.multiply(Y, np.log(Yhat)
+                     + np.multiply((1 - Y), np.log(1 - Yhat)))))
+
+    return cost
 
 
 def compute_activation_gradient(A, dA, activation_function):
@@ -103,20 +120,39 @@ def compute_linear_gradients(dZ, W, Aprev):
     return dW, db, dAprev
 
 
-def backward(dYhat, activations, model_parameters, activation_functions):
+def backward(Y, activations, model_parameters, activation_functions):
+    """Return a dictionary of gradients.  For each layer, l, greater than 1,
+    the dictionary inlcudes key, dWl, db, dA, to numpy arrays storing the
+    gradients for that layer."""
     number_of_layers = len(activations)
+
+    # Add an offset to the output layer activation to ensure that we do not
+    # divide by zero.
+    Yhat = activations['A' + str(number_of_layers - 1)] + 1e-6
+
+    # Calculate the derivative of the cost function with respect to the output
+    # layer activation.
+    activation_function = activation_functions[number_of_layers - 1]
+    if activation_function == 'sigmoid':
+        dYhat = np.divide(1 - Y, 1 - Yhat) - np.divide(Y, Yhat)
+    else:
+        pass # need to add softmax.
+
     gradients = {'dA' + str(number_of_layers - 1): dYhat}
+
+    # Propagate the derivative of the cost function with respect to the
+    # output layer activation back through the network.
     for layer in reversed(range(1, number_of_layers)):
 
-        # Obtain model parameters, activations, and gradients for backward
-        # propagation.
+        # Obtain the model parameters, activations, and gradients required for
+        # backward propagation.
         W = model_parameters['W' + str(layer)]
         activation_function = activation_functions[layer]
         A = activations['A' + str(layer)]
         Aprev = activations['A' + str(layer - 1)]
         dA = gradients['dA' + str(layer)]
 
-        # Compute gradients.
+        # Compute the gradients.
         dZ = compute_activation_gradient(A, dA, activation_function)
         dW, db, dAprev = compute_linear_gradients(dZ, W, Aprev)
 
@@ -142,16 +178,28 @@ def train(X_train, Y_train,
           number_of_iterations, learning_rate,
           print_cost=False, print_rate=250):
 
+    # Some simple error checking to be expanded upon at a later date.  Could
+    # automatically arrange data, for example.
+    assert len(layer_dimensions) > 1
+    assert len(layer_dimensions) == len(activation_functions)
+    assert len(Y_train) == layer_dimensions[-1]
+    assert len(X_train) == layer_dimensions[0]
+
     number_of_layers = len(layer_dimensions)
     model_parameters = initialise_model_parameters(layer_dimensions)
 
     # Forward and backward propagation.
     costs = [[], []]
     for iteration in range(1, number_of_iterations + 1):
-        activations = forward(X_train, model_parameters, activation_functions)
-        output_activations = activations['A' + str(number_of_layers - 1)]
-        cost, dYhat = compute_cost(output_activations, Y_train)
-        gradients = backward(dYhat, activations,
+
+        # Forward.
+        activations = forward(X_train,
+                              model_parameters, activation_functions)
+        Yhat = activations['A' + str(number_of_layers - 1)]
+        cost = compute_cost(Yhat, Y_train)
+
+        # Backward.
+        gradients = backward(Y_train, activations,
                              model_parameters, activation_functions)
         model_parameters = update_model_parameters(model_parameters,
                                                    gradients,
@@ -163,10 +211,10 @@ def train(X_train, Y_train,
             costs[1].append(cost)
             if iteration % print_rate == 0:
                 print('Cost at iteration {}: {}'.format(iteration, cost))
-            if iteration == number_of_iterations - 1:
+            if iteration == number_of_iterations:
                 fig, ax = plt.subplots()
                 ax.plot(costs[0], costs[1], c='hotpink')
-                ax.set_title('Model Training')
+                ax.set_title('Hot Pink Loss Curve!')
                 ax.set_ylabel('Cost')
                 ax.set_xlabel('Iteration')
                 fig.show()
@@ -177,45 +225,88 @@ def train(X_train, Y_train,
 def test(X_test, Y_test, model_parameters, activation_functions):
     output_layer_number = (len(model_parameters) + 2) // 2 - 1
     activations = forward(X_test, model_parameters, activation_functions)
-    output_activations = activations['A' + str(output_layer_number)].T
-    cost, _ = compute_cost(output_activations, Y_test)
-    return output_activations, cost
+    Yhat = activations['A' + str(output_layer_number)]
+    cost = compute_cost(Yhat, Y_test)
+    return Yhat, cost
 
 
 def or_gate():
-    # Y is the bitwise-or of X[0] and X[1].
-    X = np.array([[0, 1, 0, 1], [0, 0, 1, 1]])
-    Y = np.array([[0, 1, 1, 1]])
+    """
+    Train a neural network to predict the output of an OR gate.  This is
+    useful for testing code for a single classification.
+    """
+    X = np.array([[0, 1, 0, 1],
+                  [0, 0, 1, 1]])
+    Y = np.array([[0, 1, 1, 1]])    # X[0] bitwise-OR X[1]
 
     # Train a model.
     layer_dimensions = [2, 2, 1]
-    activation_functions = [None, 'relu', 'sigmoid']
+    activation_functions = [None, 'sigmoid', 'sigmoid']
     model_parameters = train(X, Y,
                              layer_dimensions, activation_functions,
                              number_of_iterations=25000, learning_rate=.05,
                              print_cost=True, print_rate=500)
 
     # Test the model.
-    output_activations, cost = test(X, Y,
-                                    model_parameters, activation_functions)
+    Yhat, _ = test(X, Y, model_parameters, activation_functions)
+    predictions = Yhat > 0.5
 
     # Print the predicted values.
-    print('\nFour cases:')
     for i in range(4):
         print(X[1][i], 'or', X[0][i], '=', Y[0][i],
-              'for which the model predicts', *output_activations[i])
+              'for which the model predicts', predictions[0][i].astype(int))
+
+
+def or_and_xor_gates():
+    """
+    Train a neural network to predict the output of OR, AND, and XOR gates.
+    This is useful for testing code for multi-class classification.
+    """
+    X = np.array([[0, 1, 0, 1],
+                  [0, 0, 1, 1]])
+    Y = np.array([[0, 1, 1, 1],     # X[0] bitwise-OR X[1]
+                  [0, 0, 0, 1],     # X[0] bitwise-AND X[1]
+                  [0, 1, 1, 0]])    # X[0] bitwise-XOR X[1]
+
+    # Train a model.
+    layer_dimensions = [2, 4, 3]
+    activation_functions = [None, 'relu', 'sigmoid']
+    model_parameters = train(X, Y,
+                             layer_dimensions, activation_functions,
+                             number_of_iterations=5000,
+                             learning_rate=.005,
+                             print_cost=True, print_rate=500)
+
+    # Test the model.
+    Yhat, _ = test(X, Y, model_parameters, activation_functions)
+    predictions = Yhat > 0.5
+
+    # Print the predicted values.
+    print()
+    for i in range(4):
+        print(X[1][i], 'OR', X[0][i], '==', Y[0][i],
+              'for which the model predicts', predictions[0][i].astype(int))
+    print()
+    for i in range(4):
+        print(X[1][i], 'AND', X[0][i], '==', Y[1][i],
+              'for which the model predicts', predictions[1][i].astype(int))
+    print()
+    for i in range(4):
+        print(X[1][i], 'XOR', X[0][i], '==', Y[1][i],
+              'for which the model predicts', predictions[2][i].astype(int))
 
 
 def digits():
 
+    # This is overfitting.  Will try adding regularization.
+
     # MNIST database.
-    file_path_to_train_csv = '/Users/Danny/Documents/Python/Digits/train.csv'
-    file_path_to_test_csv = '/Users/Danny/Documents/Python/Digits/test.csv'
-    training_data = pd.read_csv(file_path_to_train_csv)
-    test_data = pd.read_csv(file_path_to_test_csv)
+    file_path_to_csv = '/Users/Danny/Documents/Python/Digits/train.csv'
+    data = pd.read_csv(file_path_to_csv)
+    data = data.sample(frac=1)
 
     # Store each training example input in a separate column.
-    X_train = training_data.iloc[1:, 1:].T
+    X_train = data.iloc[1:4000, 1:].T
     number_of_features = len(X_train)
     m = len(X_train.iloc[0])
 
@@ -223,21 +314,29 @@ def digits():
     # encoded value in a separate column.
     Y_train = np.array([[0] * 10 for _ in range(m)]).T
     for i in range(m):
-        Y_train[training_data.iloc[i, 0]][i] = 1
+        Y_train[data.iloc[i, 0] - 1][i] = 1
 
     # Train a model.
-    layer_dimensions = [number_of_features]
-    activation_functions = [None]
+    layer_dimensions = [number_of_features, 25, 10]
+    activation_functions = [None, 'relu', 'sigmoid']
     model_parameters = train(X_train, Y_train,
                              layer_dimensions, activation_functions,
-                             number_of_iterations=10000, learning_rate=0.1,
-                             print_cost=True, print_rate=50)
+                             number_of_iterations=500, learning_rate=0.1,
+                             print_cost=True, print_rate=200)
 
     # Test the model.
-    X_test = test_data.iloc[1:, :].T
-    Y_test = np.array(test_data.iloc[1:, 0].values.reshape(1, 27999))
-    output_activations, cost = test(X_test, Y_test, model_parameters)
+    X_test = data.iloc[1:4000, 1:].T
+    Y_test = np.array([data.iloc[1:4000, 0]])
+    Y_test -= 1
+
+    output_activations, cost = test(X_test, Y_test,
+                                    model_parameters, activation_functions)
+
     predictions = np.argmax(output_activations, axis=0)
+
+    correct = np.sum(predictions == Y_test)
+    incorrect = np.sum(predictions != Y_test)
+    percent_correct = correct / (correct + incorrect) * 100
 
 
 if __name__ == '__main__':
